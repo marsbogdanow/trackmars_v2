79c510b066318f4aef23233af25ef29dbca776b6
mars <mars@172.29.45.161>
2014-04-29 10:31:19 +0700

    Added runtime logger

---
+++ TrackMars/src/ru/elifantiev/android/roboerrorreporter/Logger.java
@@ -0,0 +1,56 @@
+package ru.elifantiev.android.roboerrorreporter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.content.Context;
+import android.os.Environment;
+
+public class Logger {
+	
+	static private final String logDirTmp = "/Android/data/log/%s/files/"; 
+    static private final DateFormat formatter = new SimpleDateFormat("dd.MM.yy HH:mm");
+    static private final DateFormat fileFormatter = new SimpleDateFormat("dd-MM-yy");
+			
+	static public void log(String text) {
+        final String state = Environment.getExternalStorageState();
+        final Date dumpDate = new Date(System.currentTimeMillis());
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+
+        	final String logDir = String.format(logDirTmp, "trackmars");
+        	
+            File sd = Environment.getExternalStorageDirectory();
+            File stacktrace = new File(
+                    sd.getPath() + logDir,
+                    String.format(
+                            "out.debug",
+                            fileFormatter.format(dumpDate)));
+            
+            File dumpdir = stacktrace.getParentFile();
+            
+            boolean dirReady = dumpdir.isDirectory() || dumpdir.mkdirs();
+            
+            if (dirReady) {
+                FileWriter writer = null;
+                try {
+                    writer = new FileWriter(stacktrace, true);
+                    writer.write(text + "\n");
+                } catch (IOException e) {
+                    // ignore
+                } finally {
+                    try {
+                        if (writer != null)
+                            writer.close();
+                    } catch (IOException e) {
+                        // ignore
+                    }
+                }
+            }
+        }
+		
+	}
+}
